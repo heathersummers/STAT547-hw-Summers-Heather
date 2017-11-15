ed5da8a7dc044a85363d0e01d80f7017d03b88bd
Heather <heather.summers@alumni.ubc.ca>
2017-11-14 23:26:45 -0800

    last few changes

--- hw07/Makefile.R
+++ hw07/Makefile.R
@@ -3,14 +3,14 @@ outputs <- c("gapminder.tsv",
              "gapminder_clean.tsv",
              "gap_coefficients.tsv",
              "best_worst_countries.tsv",
-             list.files(pattern = "*.png$"))
+             list.files(pattern = "*.png$"),
+             "hw07.Rmd")
 file.remove(outputs)
 
 #run my scripts
 source("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/gapminder_exploratory.R")
 source("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/gapminder_statistical.R")
 source("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/gapminder_figures.R")
+source("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/hw07.Rmd")
 
-rmarkdown::render("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/gapminder_exploratory.R")
-rmarkdown::render("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/gapminder_statistical.R")
-rmarkdown::render("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/gapminder_figures.R")
+rmarkdown::render("C:/Users/Heather/Documents/UBC_Graduate _School/STAT_547/STAT547-hw-Summers-Heather/hw07/hw07.Rmd")
